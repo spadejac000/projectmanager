82ca299dc09545dc8707e52bc7d535ba51dac6ae
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-09-23 11:33:25 -0700

    creating component files

---
+++ src/Components/AddProject.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class AddProject extends Component {
+
+  render() {
+
+    return (
+      <div className="Projects">
+        {projectItems}
+      </div>
+    );
+  }
+}
+
+export default AddProject;
