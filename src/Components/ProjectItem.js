482bc512842b107180662fabe032488e8d6fda83
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-09-23 16:22:27 -0700

    adding todo items

--- src/Components/ProjectItem.js
+++ src/Components/ProjectItem.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ProjectItem extends Component {
+  deleteProject(id) {
+    this.props.onDelete(id);
+  }
 
   render() {
     return (
       <li className="Project">
-        <strong>{this.props.project.title}</strong>: {this.props.project.category}
+        <strong>{this.props.project.id}</strong>: {this.props.project.category} <a href="#" onClick={this.deleteProject.bind(this, this.props.project.id)}>&times;</a>
       </li>
     );
   }
+}
+
+ProjectItem.propTypes = {
+  project: PropTypes.object,
+  onDelete: PropTypes.func
 }
 
 export default ProjectItem;
